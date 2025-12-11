f3574f86422374a2e71eeff4462dfdfd4e08c788
oabgnol63 <148204963+oabgnol63@users.noreply.github.com>
2025-12-12 00:33:58 +0700

    Update setup_chrome_extension.ps1

--- setup_chrome_extension.ps1
+++ setup_chrome_extension.ps1
@@ -7,7 +7,7 @@ $destPath = "C:\Users\sauce\my_extension"
 New-Item -ItemType Directory -Force -Path "C:\sauce"
 
 # 3. Download your zipped extension (Replace with YOUR zip link)
-Invoke-WebRequest -Uri "https://github.com/oabgnol63/prer/raw/refs/heads/main/test.zip" -OutFile $zipPath
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/oabgnol63/prer/refs/heads/main/test.zip" -OutFile $zipPath
 
 # 4. Unzip it so Chrome can read the folder
 Expand-Archive -Path $zipPath -DestinationPath $destPath -Force
