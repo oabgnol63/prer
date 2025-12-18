43543b9ff82f347e43561ca4c36e9f45f7401541
oabgnol63 <148204963+oabgnol63@users.noreply.github.com>
2025-12-18 15:06:26 +0700

    Create setup_policy.bat for Chrome extension policies
    This batch file sets up Chrome extension policies by checking for admin rights, cleaning up old keys, and adding a new extension to the policy list.

---
+++ setup_policy.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+:: 1. Check for Admin rights (Exit if not Admin)
+net session >nul 2>&1
+if %errorLevel% neq 0 exit /b
+
+:: 2. Cleanup: Remove any incorrect or old keys silently
+reg delete "HKLM\SOFTWARE\Google\Chrome\ExtensionInstallForcelist" /f >nul 2>&1
+reg delete "HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist" /f >nul 2>&1
+
+:: 3. Create Policy Key
+reg add "HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist" /f >nul
+
+:: 4. Add Extension (ID;UpdateURL)
+:: Extension ID from your screenshot: ddkjiahejlhfcafbddmgiahcphecmpfh
+reg add "HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "ddkjiahejlhfcafbddmgiahcphecmpfh;https://clients2.google.com/service/update2/crx?response=redirect&prodversion=130&acceptformat=crx3&x=id%3Dddkjiahejlhfcafbddmgiahcphecmpfh%26uc" /f >nul
