1b721ea7dbb4d60c897fec0639f25ca29b009540
oabgnol63 <148204963+oabgnol63@users.noreply.github.com>
2026-01-03 15:35:33 +0700

    Update setup_policy.bat

--- setup_policy.bat
+++ setup_policy.bat
@@ -2,14 +2,20 @@
 setlocal
 
 :: 1. DEFINE VARIABLES
-:: uBlock Origin Lite ID
+:: Chrome
 set "EXT1_ID=ddkjiahejlhfcafbddmgiahcphecmpfh"
 set "EXT2_ID=edibdbjcniadpccecjdfdjjppcpchdlm"
 set "EXT3_ID=gidlfommnbibbmegmgajdbikelkdcmcl"
 set "EXT4_ID=ohahllgiabjaoigichmmfljhkcfikeof"
+
+:: Edge:
+set "EXT1_EID=cimighlppcgcoapaliogpjjdehbnofhn"
+set "EXT2_EID=kkacdgacpkediooahopgcbdahlpipheh"
+set "EXT3_EID=pciakllldcajllepkbbihkmfkikheffb"
+
 :: Standard Web Store Update URL
 set "URL=https://clients2.google.com/service/update2/crx"
-set "EDGE_SOURCES=HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallSources"
+set "EDGE_URL=https://edge.microsoft.com/extensionwebstorebase/v1/crx"
 
 :: 2. CLEANUP OLD KEYS (To avoid conflicts)
 reg delete "HKCU\Software\Google\Chrome\Extensions" /f >nul 2>&1
@@ -18,11 +24,7 @@ reg delete "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /f >
 reg delete "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /f >nul 2>&1
 
 :: 3. CREATE THE POLICY KEY (HKCU)
-:: We use "Policies" (Strong) instead of "Extensions" (Weak)
 reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /f >nul
-
-reg add "%EDGE_SOURCES%" /f >nul
-reg add "%EDGE_SOURCES%" /v "1" /t REG_SZ /d "%URL%/*" /f >nul
 reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /f >nul
 
 :: 4. ADD THE EXTENSION
@@ -32,8 +34,7 @@ reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "2" 
 reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "3" /t REG_SZ /d "%EXT3_ID%;%URL%" /f >nul
 :: reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "4" /t REG_SZ /d "%EXT4_ID%;%URL%" /f >nul
 
-reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%EXT1_ID%;%URL%" /f >nul
-reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "2" /t REG_SZ /d "%EXT2_ID%;%URL%" /f >nul
-reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "3" /t REG_SZ /d "%EXT3_ID%;%URL%" /f >nul
-:: reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "4" /t REG_SZ /d "%EXT4_ID%;%URL%" /f >nul
+reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%EXT1_EID%;%EDGE_URL%" /f >nul
+reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "2" /t REG_SZ /d "%EXT2_EID%;%EDGE_URL%" /f >nul
+reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "3" /t REG_SZ /d "%EXT3_EID%;%EDGE_URL%" /f >nul
 exit /b 0
