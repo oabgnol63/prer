dec2921fc49b24440c406245d4cdf6dc134c6339
oabgnol63 <148204963+oabgnol63@users.noreply.github.com>
2026-04-29 02:05:21 +0700

    Update setup_policy.bat

--- setup_policy.bat
+++ setup_policy.bat
@@ -7,11 +7,13 @@ set "EXT1_ID=ddkjiahejlhfcafbddmgiahcphecmpfh"
 set "EXT2_ID=edibdbjcniadpccecjdfdjjppcpchdlm"
 set "EXT3_ID=gidlfommnbibbmegmgajdbikelkdcmcl"
 set "EXT4_ID=ohahllgiabjaoigichmmfljhkcfikeof"
+set "EXT5_ID=dknlfmjaanfblgfdfebhijalfmhmjjjo"
 
 :: Edge:
 set "EXT1_EID=cimighlppcgcoapaliogpjjdehbnofhn"
 set "EXT2_EID=kkacdgacpkediooahopgcbdahlpipheh"
 set "EXT3_EID=pciakllldcajllepkbbihkmfkikheffb"
+set "EXT5_EID=dknlfmjaanfblgfdfebhijalfmhmjjjo"
 
 :: Standard Web Store Update URL
 set "URL=https://clients2.google.com/service/update2/crx"
@@ -33,8 +35,10 @@ reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "1" 
 reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "2" /t REG_SZ /d "%EXT2_ID%;%URL%" /f >nul
 reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "3" /t REG_SZ /d "%EXT3_ID%;%URL%" /f >nul
 :: reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "4" /t REG_SZ /d "%EXT4_ID%;%URL%" /f >nul
+reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "5" /t REG_SZ /d "%EXT5_ID%;%URL%" /f >nul
 
 reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%EXT1_EID%;%EDGE_URL%" /f >nul
 reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "2" /t REG_SZ /d "%EXT2_EID%;%EDGE_URL%" /f >nul
 ::reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "3" /t REG_SZ /d "%EXT3_EID%;%EDGE_URL%" /f >nul
+reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "5" /t REG_SZ /d "%EXT5_EID%;%URL%" /f >nul
 exit /b 0
