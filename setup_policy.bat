2b54e6a8a2261e2b65b85eefd4d0671226aebb57
oabgnol63 <148204963+oabgnol63@users.noreply.github.com>
2026-05-01 02:29:46 +0700

    Update setup_policy.bat

--- setup_policy.bat
+++ setup_policy.bat
@@ -32,13 +32,13 @@ reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /f >nu
 :: 4. ADD THE EXTENSION
 :: Because this is a Web Store URL, Chrome allows this even on non-enterprise machines.
 reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%EXT1_ID%;%URL%" /f >nul
-reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "2" /t REG_SZ /d "%EXT2_ID%;%URL%" /f >nul
+:: reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "2" /t REG_SZ /d "%EXT2_ID%;%URL%" /f >nul
 reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "3" /t REG_SZ /d "%EXT3_ID%;%URL%" /f >nul
 :: reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "4" /t REG_SZ /d "%EXT4_ID%;%URL%" /f >nul
 reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "5" /t REG_SZ /d "%EXT5_ID%;%URL%" /f >nul
 
 reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%EXT1_EID%;%EDGE_URL%" /f >nul
-reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "2" /t REG_SZ /d "%EXT2_EID%;%EDGE_URL%" /f >nul
-::reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "3" /t REG_SZ /d "%EXT3_EID%;%EDGE_URL%" /f >nul
+:: reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "2" /t REG_SZ /d "%EXT2_EID%;%EDGE_URL%" /f >nul
+:: reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "3" /t REG_SZ /d "%EXT3_EID%;%EDGE_URL%" /f >nul
 reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "5" /t REG_SZ /d "%EXT5_EID%;%URL%" /f >nul
 exit /b 0
